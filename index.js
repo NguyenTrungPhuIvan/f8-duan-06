4b9beb4eed388b48051367210cd23d7041b565ea
PhuNguyen <157004724+NguyenTrungPhuIvan@users.noreply.github.com>
2024-05-09 19:06:51 +0700

    question

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+// video
 const playbtn = document.querySelector(".intro__video-play");
 const introBtn = document.querySelector(".intro__video-card");
 const video = document.querySelector(".intro__video-video");
+// question
+const btnQuestio = document.querySelector(".question__item-btn");
+const itemQuestion = document.querySelector(".question__item");
+
+const btnQuestios = document.querySelectorAll(".question__item-btn");
+const itemQuestions = document.querySelectorAll(".question__item");
 
 const run = (() => {
     playVideo();
+    showContent();
 })();
 
 function playVideo() {
@@ -25,4 +33,15 @@ function playVideo() {
         introBtn.style.display = "none";
         video.play();
     };
+}
+
+function showContent() {
+    itemQuestions.forEach((item, index) => {
+        btn = btnQuestios[index];
+        btn.onclick = () => {
+            const active = document.querySelector(".question__item.active");
+            active.classList.remove("active");
+            item.classList.add("active");
+        };
+    });
 }
